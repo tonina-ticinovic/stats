2f5438e48383211415e5f2be3e2969fdd22169b7
Tonina Tičinović <96192639+tonina-ticinovic@users.noreply.github.com>
2024-11-27 21:00:20 +0100

    Use enum for match result (#4)

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,18 @@ const matches = fs
   .split('\n')
   .map((row) => row.split(','));
 
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
   if (
-    (match[1] === 'Man United' && match[5] === 'H') ||
-    (match[2] === 'Man United' && match[5] === 'A')
+    (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
+    (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
   ) {
     manUnitedWins++;
   }
